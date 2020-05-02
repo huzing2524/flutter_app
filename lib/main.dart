a754c236ef6238939f6f218f76b13f3da14846b9
huzing2524 <826956010@qq.com>
2020-05-02 11:07:38 +0800

    upload 2020.5.2

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+//          child: new Text('Hello World'),
+          child: new Column(
+              children: <Widget>[
+                new Text('我叫罗黎明！'),
+                new Text('我是会长！')]),
+        ),
+      ),
+    );
+  }
+}
